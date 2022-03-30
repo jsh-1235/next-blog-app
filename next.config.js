41e9e9e4f186aa6094b6ddc46d5542c671d2a291
jsh-1235 <>
2022-03-30 15:32:37 +0900

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
-  poweredByHeader: process.env.NODE_ENV === "development",
-  reactStrictMode: process.env.NODE_ENV === "development",
+  distDir: "build",
+  reactStrictMode: true,
+  assetPrefix: "https://~~~",
+  // poweredByHeader: process.env.NODE_ENV === "development",
+  // reactStrictMode: process.env.NODE_ENV === "development",
 };
 
 module.exports = nextConfig;
